3ff17ce19a2e55b1e275aa0ef5ce9aed2c9d48fe
Bermet <oceaninapuddle@gmail.com>
2023-01-23 22:31:56 +0600

    collections created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Collection } from "./Collection";
 import "./index.scss";
 
+const places = [
+  { name: "Все" },
+  { name: "Море" },
+  { name: "Горы" },
+  { name: "Архитектура" },
+  { name: "Города" },
+];
+
 function App() {
+  const [categoryId, setCategoryId] = useState(0);
   const [searchValue, setSearchValue] = useState("");
   const [collections, setCollections] = useState([]);
 
@@ -23,11 +32,16 @@ function App() {
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {/* <li className="active">Все</li> */}
+          {places.map((obj, i) => (
+            <li
+              onClick={() => setCategoryId(i)}
+              className={categoryId === i ? "active" : ""}
+              key={obj.name}
+            >
+              {obj.name}
+            </li>
+          ))}
         </ul>
         <input
           value={searchValue}
@@ -38,7 +52,9 @@ function App() {
       </div>
       <div className="content">
         {collections
-          .filter((obj) => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
+          .filter((obj) =>
+            obj.name.toLowerCase().includes(searchValue.toLowerCase())
+          )
           .map((obj, index) => (
             <Collection key={index} name={obj.name} images={obj.photos} />
           ))}
